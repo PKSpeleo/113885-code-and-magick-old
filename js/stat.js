62e1906e913a44ee56f7f69919fa9f4f55a4f35d
Pavel Katkov <pkspeleo@gmail.com>
2017-08-23 12:37:02 +0300

    перенес переменные

--- js/stat.js
+++ js/stat.js
@@ -1,37 +1,38 @@
 // stat.js
 'use strict';
-// Переменные для облака
-var cloudInitialX = 100;
-var cloudInitialY = 10;
-var cloudWidth = 420;
-var cloudHeight = 270;
-var cloudColour = 'rgba(0, 0, 0, 0.7)';
-// Переменные текста облака
-var cloudText = 'Ура вы победили!\nСписок результатов:';
-var cloudTextColour = '#000';
-var cloudTextStyle = '16px PT Mono';
-var cloudTextInitialX = 120;
-var cloudTextInitialY = 40;
-var cloudTextHeightInPX = 20;
-
-// Переменные для тени
-var cloudShadowOffsetX = 10;
-var cloudShadowOffsetY = 10;
-var cloudShadowColour = 'rgba(255, 255, 255, 1.0)';
-
-// Переменные для гистораммы
-var histogramHeight = 150;
-var histogramColumnWidth = 40;
-var histogramColumnSpace = 50;
-var histogramInitialX = 120;
-var histogramInitialY = 100;
-var histogramMyColumnName = 'Вы';
-var histogramMyColumnColour = 'rgba(255, 0, 0, 1.0)';
-var histogramColumnColourPart = 'rgba(0, 0, 255, ';
-var histogramTetxUpSffset = 10;
-
 
 window.renderStatistics = function (ctx, names, times) {
+  // Переменные для облака
+  var cloudInitialX = 100;
+  var cloudInitialY = 10;
+  var cloudWidth = 420;
+  var cloudHeight = 270;
+  var cloudColour = 'rgba(0, 0, 0, 0.7)';
+  // Переменные текста облака
+  var cloudText = 'Ура вы победили!\nСписок результатов:';
+  var cloudTextColour = '#000';
+  var cloudTextStyle = '16px PT Mono';
+  var cloudTextInitialX = 120;
+  var cloudTextInitialY = 40;
+  var cloudTextHeightInPX = 20;
+
+  // Переменные для тени
+  var cloudShadowOffsetX = 10;
+  var cloudShadowOffsetY = 10;
+  var cloudShadowColour = 'rgba(255, 255, 255, 1.0)';
+
+  // Переменные для гистораммы
+  var histogramHeight = 150;
+  var histogramColumnWidth = 40;
+  var histogramColumnSpace = 50;
+  var histogramInitialX = 120;
+  var histogramInitialY = 100;
+  var histogramMyColumnName = 'Вы';
+  var histogramMyColumnColour = 'rgba(255, 0, 0, 1.0)';
+  var histogramColumnColourPart = 'rgba(0, 0, 255, ';
+  var histogramTetxUpSffset = 10;
+
+
   // Функция рисования окна статистики
   var drawStatCloud = function (x, y, width, height, colour) {
     ctx.fillStyle = colour;
